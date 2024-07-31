2fd1666f7306fa3458bff7e814a92cb87d18683d
YootTanA <oatyootna@gmail.com>
2024-07-31 18:40:11 +0700

    add reading file for visualizer

--- visualizer/app/page.tsx
+++ visualizer/app/page.tsx
@@ -1,14 +1,23 @@
+import UserBox from "@/component/UserBox";
 import Image from "next/image";
+import { promises as fs } from "fs";
 
-export default function Home() {
+async function getUsers() {
+  try {
+    const file = await fs.readFile("../../user.json", "utf8");
+    return JSON.parse(file);
+  } catch {
+    return {};
+  }
+}
+
+export default async function Home() {
+  const data = await getUsers();
+  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="grid grid-cols-10 gap-5">
-          <div className="w-fit h-fit border-2 border-black items-center flex justify-center p-5 text-base">
-            user-1
-          </div>
-        </div>
+        <div className="grid grid-cols-10 gap-5"></div>
       </div>
     </main>
   );
