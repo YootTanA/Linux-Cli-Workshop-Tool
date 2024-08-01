65a25e0adc28f2e7c49574c93b674cb86646de6d
YootTanA <oatyootna@gmail.com>
2024-08-01 15:08:16 +0700

    fixed bug

---
+++ visualizer/app/api/users/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import { promises as fs } from "fs";
+
+let content = "";
+const filePath = "database/user.json";
+
+const readFile = async () => {
+  try {
+    content = await fs.readFile(filePath, "utf8");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const chokidar = require("chokidar");
+const watcher = chokidar.watch(filePath, { persistent: true });
+
+watcher.on("change", () => {
+  console.log("File changed, updating cache...");
+  readFile();
+});
+
+// Initial read
+readFile();
+
+export async function GET() {
+  return NextResponse.json(JSON.parse(content));
+}
